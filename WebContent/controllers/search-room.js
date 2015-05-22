00ae23cb3e360109620794a15ee6084ebcff507d
farahnaj <farahinamdar@gmail.com>
2015-05-22 14:47:04 +0530

    mobile view done for some pages

--- WebContent/controllers/search-room.js
+++ WebContent/controllers/search-room.js
@@ -60,4 +60,25 @@
          // Show slot in slot detail view
          window.location = '#/availablerooms';
      };
+     
+     
+     
+    toggle = 0;
+ 	$jQ('#toggle-filter').click(function(e) {
+ 		e.preventDefault();
+ 		if (toggle == 0) {
+ 			$jQ('#filters-column').animate({
+ 				'right' : '0px'
+ 			});
+ 			toggle = 1;
+ 			return;
+ 		}
+
+ 		else {
+ 			$jQ('#filters-column').animate({
+ 				'right' : '-300px'
+ 			});
+ 			toggle = 0;
+ 			return;
+ 		}});
  });
